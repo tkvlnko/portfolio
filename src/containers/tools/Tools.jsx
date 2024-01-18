5bb9f189f7bb34f84e47c7beb32398416172fb7d
Taya <tayaibragimova@gmail.com>
2024-01-18 23:59:16 +0300

    fix

--- src/containers/tools/Tools.jsx
+++ src/containers/tools/Tools.jsx
@@ -5,10 +5,10 @@ const Tools = () => {
     <div className="section__padding section__margin">
       <div className="px-[8rem] py-[8rem] 
                             lg:px-[4rem] sm:px-[2rem] sm:py-[40%] lg:py-[30%]
-                            flex flex-col justify-start align-center">
+                            flex flex-col justify-center align-center text-center">
         <h1 className="text-2xl mb-10">Some technologies I&apos;ve been working with:</h1>
         <div className="flex justify-center w-full">
-        <div className="flex py-[4rem] px-[2rem] flex-wrap w-[50%] justify-center border">
+        <div className="flex py-[4rem] px-[2rem] flex-wrap w-[50%] justify-center border rounded-xl border-violet-300">
             {Object.keys(icons).map((iconName) => (
               <img key={iconName} src={icons[iconName]} alt={iconName} width="60" height="60" className="img-item m-3"/>
             ))}
