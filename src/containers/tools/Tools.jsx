4f0eef37c55abfdf799a7e4415a9330cd51f702e
Taya <tayaibragimova@gmail.com>
2024-01-23 23:57:49 +0300

    fix

--- src/containers/tools/Tools.jsx
+++ src/containers/tools/Tools.jsx
@@ -1,5 +1,5 @@
 import { icons } from "../../assets/icons";  // Assuming all icons are exported from assets.js as a single object
-
+import "./Tools.css"
 const Tools = () => {
   return (
     <div className="section__padding section__margin">
@@ -8,10 +8,17 @@ const Tools = () => {
                             flex flex-col justify-center align-center text-center">
         <h1 className="text-2xl mb-10">Some technologies I&apos;ve been working with:</h1>
         <div className="flex justify-center w-full">
-        <div className="flex py-[4rem] px-[2rem] flex-wrap w-[50%] justify-center border rounded-xl border-violet-300">
-            {Object.keys(icons).map((iconName) => (
-              <img key={iconName} src={icons[iconName]} alt={iconName} width="60" height="60" className="img-item m-3"/>
-            ))}
+        <div className="flex py-[4rem] px-[2rem] flex-wrap w-[50%] sm:w-full justify-center border rounded-xl border-violet-300">
+        {Object.keys(icons).map((iconName) => (
+          <div className="img-item m-3 sm:m-1" key={iconName}>
+            <img  
+              src={icons[iconName]} 
+              alt={iconName} 
+              width="60" 
+              height="60"
+            />
+          </div>
+        ))}
           </div>
         </div>
       </div>
